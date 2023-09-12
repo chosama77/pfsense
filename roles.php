8507df42692e8c719f022cfa832c51496233525c
chosama77 <118628341+chosama77@users.noreply.github.com>
2023-09-12 00:16:54 -0700

    Add files via upload

---
+++ roles.php
@@ -0,0 +1,154 @@
+<?php
+    $jsonDataArray = [
+        '{
+          "code": 200,
+          "status": "OK",
+          "data": [
+              {
+                  "zone": "netcue",
+                  "descr": "its netcue",
+                  "localauth_priv": "",
+                  "zoneid": "2",
+                  "interface": "wan",
+                  "roll": [
+                      {
+                          "zone": "netcue",
+                          "number": "1",
+                          "minutes": "180",
+                          "descr": "Comment",
+                          "count": "100",
+                          "used": "AAAAAAAAAAAAAAAAAA==",
+                          "active": "",
+                          "lastsync": "1694156834"
+                      },
+                      {
+                          "zone": "netcue",
+                          "number": "2",
+                          "minutes": "240",
+                          "descr": "Comment 2",
+                          "count": "200",
+                          "used": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+                          "active": "",
+                          "lastsync": "1694156844"
+                      },
+                      {
+                          "zone": "netcue",
+                          "number": "0",
+                          "minutes": "1",
+                          "comment": "New Voucher Created",
+                          "descr": "Description",
+                          "count": "10",
+                          "used": "AAA=",
+                          "active": ""
+                      }
+                  ]
+              },
+              {
+                  "zone": "Gold",
+                  "descr": "Gold Zone",
+                  "localauth_priv": "",
+                  "zoneid": "4",
+                  "interface": "lan",
+                  "roll": [
+                      {
+                          "zone": "gold",
+                          "number": "0",
+                          "minutes": "181",
+                          "descr": "",
+                          "count": "182",
+                          "used": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+                          "active": "",
+                          "lastsync": "1694426414"
+                      }
+                  ]
+              }
+          ]
+        }',
+      ];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Rolls</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid px-5">
+    <a class="navbar-brand" href="#">Pfsense</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid my-3" style="display: flex; justify-content: center;">
+<div class="card" style="background-color: #F8F9FA !important; width: 95%;">
+  <div class="card-body" style="padding: 10px !important;">
+   <h4>Rolls</h4>
+  </div>
+</div>
+</div>
+
+<div class="container my-3">
+<table class="table table-bordered">
+<thead>
+    <tr>
+      <th>Roll Number</th>
+      <th>Roll Minutes</th>
+      <th>Roll Description</th>
+      <th>Roll Count</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    if (isset($_GET['zone'])) {
+      $zone = $_GET['zone'];
+
+      // Find the data for the selected zone
+      foreach ($jsonDataArray as $json) {
+        $data = json_decode($json, true);
+
+        if ($data !== null) {
+          foreach ($data['data'] as $entry) {
+            if ($entry['zone'] === $zone) {
+              foreach ($entry['roll'] as $roll) {
+                echo "<tr>";
+                echo "<td>{$roll['number']}</td>";
+                echo "<td>{$roll['minutes']}</td>";
+                echo "<td>{$roll['descr']}</td>";
+                echo "<td>{$roll['count']}</td>";
+                echo "<td><button class='btn btn-primary'>edit</button> <button class='btn btn-danger'>delete</button></td>";
+                echo "</tr>";
+              }
+            }
+          }
+        } else {
+          echo "<tr><td colspan='7'>Error decoding JSON data.</td></tr>";
+        }
+      }
+    }
+    ?>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
